1b5ebd595c316397cfc6b782bade65f52fd3156d
octavio mejia <arkord1@gmail.com>
2021-02-24 21:24:19 -0600

    meter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,19 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    process.stdout.write("\u001b[2J\u001b[0;0H");
+    const jm = require('js-meter')
+
+    const isPrint = true
+    const isKb = true 
+
+    const m = new jm({isPrint, isKb})
+
+    for(var i=0; i<1000000000; i++){
+        Math.random()
+    }
+
+    const meter = m.stop()
     return this.appService.getHello();
   }
 }
